b1a19a6242a8e5d98df7c50ac0272f87977566bc
Juan Jose Ramirez <jjramirez@miuandes.cl>
2021-09-27 17:41:45 -0700

    quiz, timer working. startBtn almost

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,14 @@
-var questionDisplay = document.querySelector("#question")
-var answer1Display = document.querySelector("#aswer1")
-var answer2Display = document.querySelector("#aswer2")
-var answer3Display = document.querySelector("#aswer3")
-var answer4Display = document.querySelector("#aswer4")
+var questionDisplay = document.querySelector("#question");
+var answer1Display = document.querySelector("#answer1");
+var answer2Display = document.querySelector("#answer2");
+var answer3Display = document.querySelector("#answer3");
+var answer4Display = document.querySelector("#answer4");
+var quizBox = document.querySelector(".quiz");
+var quizTimer = document.querySelector("#timerCountdown");
+var startBtn = document.querySelector("#start-btn")
+var score = 0;
+var timerCountdown = 60;
+var actualQuizQuestion = 0;
 
 var questionsArray = [
     {
@@ -44,9 +50,36 @@ var questionsArray = [
 
 var questionArrayLenght = questionsArray.length
 
+startBtn.addEventListener("click", startQuiz)
+
+quizBox.addEventListener("click", function(event){
+    var element = event.target;
+    
+
+    if (element.matches(".answerBox")){
+        var answerNumber = element.getAttribute("answer-number")
+        // console.log(answerNumber);
+         var answerChecked = checkAnswer(answerNumber, actualQuizQuestion);
+         if(answerChecked){
+             score++
+         } else {
+            timerCountdown -= 5
+         }
+        nextQuestion();
+    }
+});
+
+function checkAnswer(answerElement, questionJSON){
+    if (answerElement == questionJSON.correctAnswerIndex ){
+        return true
+    } else {
+        return false
+    }
+};
+
 function randomQuestion() {
-    var randomQuestionJSON = questionsArray[Math.floor(Math.random() * questionArrayLenght)]
-    return randomQuestionJSON // Q, A1, A2, A3, A4, CA
+    var randomQuestionJSON = questionsArray[Math.floor(Math.random() * questionArrayLenght)];
+    return randomQuestionJSON; // Q, A1, A2, A3, A4, CA
 };
 
 function displayRandomQuestion(questionJSON) {
@@ -55,6 +88,62 @@ function displayRandomQuestion(questionJSON) {
     answer2Display.textContent = "2. " + questionJSON.answer2;
     answer3Display.textContent = "3. " + questionJSON.answer3;
     answer4Display.textContent = "4. " + questionJSON.answer4;
+};
+
+function changeState() {
+    quizBoxState = quizBox.getAttribute("data-state");
+    if (quizBoxState == "hidden"){
+        quizBox.setAttribute("data-state", "shown")
+        quizBox.style.display = "block";
+        
+        quizTimer.setAttribute("data-state", "shown")
+        quizTimer.style.display = "block";
+
+    } else if (quizBoxState == "shown"){
+        quizBox.setAttribute("data-state", "hidden");
+        quizBox.style.display = "none";
+
+        quizTimer.setAttribute("data-state", "hidden");
+        quizTimer.style.display = "none";
+    }
+};
+
+function changeStartBtnState() {
+    startBtnState = startBtn.getAttribute("data-state");
+    if (startBtnState == "hidden"){
+        startBtn.setAttribute("data-state", "shown")
+        startBtn.style.display = "block";
+    } else if (startBtnState == "shown"){
+        startBtn.setAttribute("data-state", "hidden");
+        startBtn.style.display = "none";
+    }
 }
 
-displayRandomQuestion(randomQuestion());
+function startQuiz(){
+    actualQuizQuestion = randomQuestion();
+    displayRandomQuestion(actualQuizQuestion);
+    changeState();
+    changeStartBtnState();
+    setTimer();
+};
+
+function nextQuestion(){
+    actualQuizQuestion = randomQuestion();
+    displayRandomQuestion(actualQuizQuestion);
+};
+
+function setTimer(){
+    timerCountdown = 20;
+
+    var timerInterval = setInterval(function() {
+        timerCountdown--;
+        quizTimer.textContent = timerCountdown + " secs"
+
+        if(timerCountdown < 0){
+            clearInterval(timerInterval)
+
+            changeState();
+            //TODO: stop quiz
+        }
+    }, 1000);
+}
